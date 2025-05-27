e6e2db5ec6726784d00b6310f71c19f1e79f2eb8
TheStupidPythonGuy <rio24110@bbi.school.nz>
2025-05-27 16:03:04 +1200

    Python

---
+++ Letter Guessing game.py
@@ -0,0 +1,246 @@
+while True:
+   score=0
+   import random
+ 
+ 
+   randomletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+   letter=(randomletter[random.randint(1, 26)])
+   while True:
+    user_input = input("You:  ")
+    while True:
+
+      if user_input == "b":
+         if letter == "b":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "c":
+         if letter == "c":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "a":
+         if letter == "a":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      if user_input == "d":
+         if letter == "d":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "e":
+         if letter == "e":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "f":
+         if letter == "f":
+            print("You got it in " + str(score) + " tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+      if user_input == "g":
+         if letter == "g":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "h":
+         if letter == "h":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "i":
+         if letter == "i":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      if user_input == "j":
+         if letter == "j":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "k":
+         if letter == "k":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "l":
+         if letter == "l":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      if user_input == "m":
+         if letter == "m":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "n":
+         if letter == "n":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "o":
+         if letter == "o":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+      if user_input == "p":
+         if letter == "p":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "q":
+         if letter == "q":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "r":
+         if letter == "r":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      if user_input == "s":
+         if letter == "s":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "t":
+         if letter == "t":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "u":
+         if letter == "u":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "v":
+         if letter == "v":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      if user_input == "w":
+         if letter == "w":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "x":
+         if letter == "x":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      elif user_input == "y":
+         if letter == "y":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+      
+      elif user_input == "z":
+         if letter == "z":
+            print("You got it in" + str(score) + "tries!")
+            break
+         else:
+            print("Wrong")
+         score = score + 1
+         break
+
+      else:
+         print("Please enter a letter in the English Alphabet (from a - z)")
+         break
